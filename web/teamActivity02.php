9a2d0f0a7ed6822e2f27f4f8814b57c1853a1aec
Christopher David <dav08049@byui.edu>
2018-01-17 14:53:28 -0700

    updated stylesheet

--- web/teamActivity02.php
+++ web/teamActivity02.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>02 Teach : Team Activity</title>
     <link href="css/style.css" type="text/css" rel="stylesheet"/>
-    <script src="js/myscripts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/myscripts.js"></script>
 </head>
     <body>
         <div class="wrapper"><!--background image applied here-->
