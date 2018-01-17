444d6761139504247211235639b54a3a2b526757
Christopher David <dav08049@byui.edu>
2018-01-16 22:51:06 -0700

    updated js file

--- web/js/myscripts.js
+++ web/js/myscripts.js
@@ -4,6 +4,8 @@ function displayClicked() {
 
 function displayColor() {
    var colorChange = document.getElementsByName("clrName").value;
+   print "colorChange";
+   exit;
    var changeThis = document.getElementById("sectionChangingColor");
    changeThis.style.backgroundColor  = colorChange;
 }
