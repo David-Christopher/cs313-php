043da11f0edf262bc5dfefbb44665402a9da3fcf
Christopher David <dav08049@byui.edu>
2018-01-18 17:09:52 -0700

    updated stylesheet

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Christopher David | Web Design & Development</title>
     <link href="css/personalStyle.css" type="text/css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
     <body>
         <div class="wrapper"><!--background image applied here-->
