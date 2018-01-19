7f9731bcd7d96a1350e2bcf0c902fa85ad250262
Christopher David <dav08049@byui.edu>
2018-01-19 14:29:20 -0700

    updated stylesheet

--- web/index.php
+++ web/index.php
@@ -42,7 +42,7 @@
                 			<h3>About</h3>
                 		</div>
                 		<div id="center">
-                			<p></p>
+                			<p>Northern California-born, Christopher David is well acquainted with beautiful sceneries. Lake Tahoe is a specific treasure trove of design inspiration. Christopher is a senior at BYU-I with a few years of web design and development field experience. Graduating in April will open new doors and adventures as he steps into the career field bringing with him a seasoned portfolio of skills and talents. </p>
                 		</div>
                 		<div id="right">
                 			<img src="images/print.png">
