3ee09823af0c346b735678bf410187ddad7518ad
Christopher David <dav08049@byui.edu>
2018-01-18 17:49:21 -0700

    updated stylesheet

--- web/index.php
+++ web/index.php
@@ -6,16 +6,30 @@
     <title>Christopher David | Web Design & Development</title>
     <link href="css/personalStyle.css" type="text/css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
     <body>
         <div class="wrapper"><!--background image applied here-->
             <div class="page-content-container"><!--content container applied here-->
                 <header>
                 	<div class="nav-background">
-	                	<nav>
-	                		<li><a href="https://vast-savannah-73411.herokuapp.com/">Introduction</a></li>
-	                		<li><a href="https://vast-savannah-73411.herokuapp.com/assignments.php">Assignments</a></li>
-	                	</nav>
+	                	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+						   <div class="collapse navbar-collapse" id="navbarNav">
+						    <ul class="navbar-nav">
+						      <li class="nav-item active">
+						        <a class="nav-link" href="https://vast-savannah-73411.herokuapp.com/">Introduction</a>
+						      </li>
+						      <li class="nav-item">
+						        <a class="nav-link" href="https://vast-savannah-73411.herokuapp.com/assignments.php">Assignments</a>
+						      </li>
+						    </ul>
+						  </div>
+						</nav>
 	               	</div>
                 	<img id="logo" src="images/CDlogo.png">
                 </header>
