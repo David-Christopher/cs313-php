e5ec6c35d105f51bde6303aa57903e90ab4a2dd0
Christopher David <dav08049@byui.edu>
2018-01-24 13:23:01 -0700

    Added senior project

---
+++ web/seniorProject/site_plan.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Gem: Site Plan</title>
+    <link href="css/style_guide.css" type="text/css" rel="stylesheet"/>
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Work+Sans" rel="stylesheet">
+</head>
+    <body>
+        <div class="wrapper"><!--background image applied here-->
+            <div class="page-content-container"><!--content container applied here-->
+                <header>
+                    <img src="images/CDlogo.png" alt="Custom logo">
+                    <nav>
+                        <div>
+                            <a href="#desc">Site Description</a><br><br>
+                            <a href="#name">Site Name</a><br><br>
+                            <a href="#aud">Target Audience</a><br><br>
+                            <a href="#pers">Personas & Scenarios</a><br><br>
+                            <a href="#list">Content List</a><br><br>
+                            <a href="#map">Site Map</a><br><br>            
+                            <a href="#guide">Style Guide</a><br><br>
+                            <a href="#wire">Page Wireframes</a><br><br>
+                            <a href="#sktch">Responsive Sketches</a><br><br>
+                        </div>
+                    </nav>
+                </header>
+                <main>
+                    <h1>Portfolio Gem: Site Plan</h1>
+                    <h2 id="desc">Site Description</h2>
+                    <div>
+                        <p>The series of web pages discussed in this proposal document will be designed for the purpose of
+                        satisfying the Capstone Project Proposal requirements at BYU-I. The web pages will be compiled
+                        under a single marketing theme to illustrate the web design and development knowledge I have
+                        gained and my capacity or skillset to learn new technologies in the web industry. This document
+                        outlines the technologies or tools that will be used, web page details, and project timelines.</p>
+
+                        <p>These web pages are intended to be used for an online business built on a drop-shipping business
+                        model. The product showcased will be manufactured and shipped by a third party. The sole responsibility of this online business is to build online presence for our brand, market to our target
+                        audience, and allow our customers to make purchases through the website.</p>
+                        
+                        <p>The site will not be published until project completion. At that time, the site will be created under a
+                        business name and license, third party relationships secured with World Wide Brands, and a fully functioning shopping cart and payment center implemented.</p>
+                    </div>
+                    <h2 id="name">Site Name</h2>
+                    <div>
+                        <p class="site_name">Dream Wedding</p>
+                        <p class="site_name subname">Decorations & Supplies</p>
+
+                    </div>
+                    <h2 id="aud">Target Audience</h2>
+                    <div>
+                        <p>Our target audience will be focused on brides & grooms, their parents, and individuals planning a wedding. The audience we wish to target will be those in search of wedding products. The site will primarily attract a younger female crowd, which will be reflected in the site design, colors, and fonts.</p>
+                    </div>
+                    <h2 id="pers">Personas & Scenarios</h2>
+                    <div>
+                        <p>Brittany</p>
+                        <ul>
+                            <li>Age: 23</li>
+                            <li>Occupation: Student</li>
+                            <li>Getting married in 5 months</li>
+                            <li>Lives in Logan, Utah</li>
+                            <li>Parents have budget of $8,000 for wedding</li>
+                            <li>$1,200 reserved for decorations and supplies</li>
+                        </ul>
+                        <p>Sarah</p>
+                        <ul>
+                            <li>Age: 43</li>
+                            <li>Occupation: Teacher</li>
+                            <li>Married with 3 kids and a daughter getting married in 12 months</li>
+                            <li>Lives in Denver, Colorado</li>
+                            <li>Owns small home with moderate mortgage</li>
+                            <li>Total budget for wedding $5,000</li>
+                            <li>$1,800 reserved for decorations and supplies</li>
+                        </ul>
+                        <p>Rebecca</p>
+                        <ul>
+                            <li>Age: 20</li>
+                            <li>Occupation: Server & Student</li>
+                            <li>Getting married in 3 months</li>
+                            <li>Lives in Rexburg, Idaho</li>
+                            <li>Total budget for wedding $3,500 split between her and her parents</li>
+                            <li>$750 reserved for decorations and supplies</li>
+                        </ul>
+                        <p>Stephanie</p>
+                        <ul>
+                            <li>Age: 28</li>
+                            <li>Occupation: Physician</li>
+                            <li>Getting married in 15 months</li>
+                            <li>Lives in Omaha, Nebraska</li>
+                            <li>Total budget for wedding $50,000</li>
+                            <li>$8,000 reserved for decorations and supplies</li>
+                        </ul>                             
+                    </div>
+                    <h2 id="list">Content List</h2>
+                    <div>
+                        <p>Home</p>
+                        <ul>
+                            <li>Main banner with product display and season based message</li>
+                            <li>Style variations category display</li>
+                            <li>Top trending products display</li>
+                        </ul>
+                        <p>Product</p>
+                        <ul>
+                            <li>Product image(s)</li>
+                            <li>Product description</li>
+                            <li>Quanity text box</li>
+                            <li>Add to cart button</li>
+                        </ul>
+                        <p>Cart</p>
+                        <ul>
+                            <li>Product image(s)</li>
+                            <li>Small description</li>
+                            <li>Quanity text box</li>
+                            <li>Remove or update quantity option</li>
+                            <li>Product totals displayed</li>
+                            <li>Subtotal displayed</li>
+                            <li>Proceed to checkout button</li>
+                        </ul>
+                        <p>Contact Us</p>
+                        <ul>
+                            <li>Email displayed</li>
+                            <li>Support statement</li>
+                            <li>Frequently asked questions</li>
+                        </ul>
+                        <p>Sign-In</p>
+                        <ul>
+                            <li>Email text field</li>
+                            <li>Password text field</li>
+                            <li>Sign-In button</li>
+                            <li>Forgot password button</li>
+                        </ul>
+                        <p>Register</p>
+                        <ul>
+                            <li>First name text field</li>
+                            <li>Last name text field</li>
+                            <li>Email text field</li>
+                            <li>Password text field</li>
+                            <li>Re-enter password text field</li>
+                            <li>Create account button</li>
+                        </ul>
+                        <p>Checkout</p>
+                        <ul>
+                            <li>Probably a third party chekcout like 1shoppingcart.com</li>
+                            <li>This page will be apart of the launching stage after project completion</li>
+                        </ul>
+                    </div>
+                    <h2 id="map">Site Map</h2>
+                    <div>
+                        <img class="site_map" src="images/siteMap.JPG" alt="Map for website pages">
+                    </div>
+                    <h2 id="guide">Style Guide</h2>
+                    <div class="style_guide">
+                        <h3>>Font Example</h3>
+                        <h1>h1 in Roboto Slab Serif</h1>
+                        <h2>h2-h4 in Work Sans-Serif</h2>
+                        <p>p in Roboto Slab Serif</p>
+                        <h3>>Color Pallet</h3>
+                        <img src="images/colorScheme.JPG" alt="Custom color pallet">
+                        <h3>>Button Example</h3>
+                        <button class="firstButton">Find Your Wedding Style</button>
+                        <button class="secondButton">Dream of Your Wedding</button>
+                    </div>
+                    <h2 id="wire">Page Wireframes</h2>
+                    <div class="Wireframes">
+                        <h3>>Home</h3>
+                        <img src="images/homeWireframe.png" alt="Wireframes">
+                        <h3>>Home: Style Product's View Expanded</h3>
+                        <img src="images/homeProductsExpanded.png" alt="Wireframes">
+                        <h3>>Products</h3>
+                        <img src="images/productsWireframe.png" alt="Wireframes">
+                        <h3>>Cart</h3>
+                        <img src="images/cartWireframe.png" alt="Wireframes">
+                        <h3>>Contact Us</h3>
+                        <img src="images/contactusWireframe.png" alt="Wireframes">
+                        <h3>>Sign-In</h3>
+                        <img src="images/singinWireframe.png" alt="Wireframes">
+                        <h3>>Register</h3>
+                        <img src="images/registerWireframe.png" alt="Wireframes">                       
+                    </div>
+                    <h2 id="sktch">Responsive Sketch</h2>
+                    <div>
+                        <h3>>Home</h3>
+                        <img src="images/homeResponsive.png" alt="Responsive Design">
+                        <h3>>Home: Style Product's View Expanded</h3>
+                        <img src="images/homeExpandedResponsive.png" alt="Responsive Design">  
+                    </div> 
+                </main>
+                <footer></footer>
+            </div><!--end of content container tag-->
+        </div>
+    </body>
+</html>
