ddd9cd403302d8c49eb67236d6e2314bda144d6a
Christopher David <dav08049@byui.edu>
2018-01-25 15:57:26 -0700

    added php shopping cart pages

--- web/assignments/phpShoppingCart/index.php
+++ web/assignments/phpShoppingCart/index.php
@@ -5,15 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Shopping Cart</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	<link href="../../../css/personalStyle.css" type="text/css" rel="stylesheet"/>
 	<script src="js/personalScripts.js"></script>
-	<link rel="icon" type="../../../image/png" href="images/favicon.png">
+	<link rel="icon" type="image" href="../../../images/favicon.png">
 </head>
     <body>
         <div class="wrapper"><!--background image applied here-->
@@ -21,8 +15,8 @@
                 <header>
                 	<div class="nav-background">
 	                	<nav class="topnav" id="myTopnav">
-						  <a href="https://vast-savannah-73411.herokuapp.com/" class="active">Introduction</a>
 						  <a href="https://vast-savannah-73411.herokuapp.com/assignments.php">Assignments</a>
+                          <a href="https://vast-savannah-73411.herokuapp.com/assignments/phpShoppingCart/cart.php" class="active">Cart</a>
 						  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="topNavFunction()">&#9776;</a>
 						</nav>
 	               	</div>
@@ -36,11 +30,8 @@
                 			<h3>About</h3>
                 		</div>
                 		<div id="center">
-                			<p>Northern California-born, Christopher David is well acquainted with beautiful sceneries. Lake Tahoe is a specific treasure trove of design inspiration. Christopher is a senior at BYU-I with a few years of web design and development field experience. Graduating in April will open new doors and adventures as he steps into the career field bringing with him a seasoned portfolio of skills and talents. </p>
+                			<p>This page is designed to simulate a php shopping cart</p>
                 		</div>
-                		<div id="right">
-                			<a href="/images/Resume1-19-18.pdf" download><img src="../../../images/print.png" alt="Custom Print Resume Icon"></a>
-                			<a href="/images/Resume1-19-18.pdf" download><p>Print Resume</p></a>
                 		</div>
                 	</div>
                 </footer>
