dcac8233d52f999cc1e7c32b641f804a1199578c
Christopher David <dav08049@byui.edu>
2018-01-25 10:08:06 -0700

    added php shopping cart pages

---
+++ web/assignments/phpShoppingCart/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Christopher David | Web Design & Development</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<link href="../../../css/personalStyle.css" type="text/css" rel="stylesheet"/>
+	<script src="js/personalScripts.js"></script>
+	<link rel="icon" type="../../../image/png" href="images/favicon.png">
+</head>
+    <body>
+        <div class="wrapper"><!--background image applied here-->
+            <div class="page-content-container"><!--content container applied here-->
+                <header>
+                	<div class="nav-background">
+	                	<nav class="topnav" id="myTopnav">
+						  <a href="https://vast-savannah-73411.herokuapp.com/" class="active">Introduction</a>
+						  <a href="https://vast-savannah-73411.herokuapp.com/assignments.php">Assignments</a>
+						  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="topNavFunction()">&#9776;</a>
+						</nav>
+	               	</div>
+                	<img id="logo" src="../../../images/CDlogo.png" alt="David Design Logo">
+                </header>
+                <main>
+                	<img id="main_img" src="../../../images/lake-tahoe.jpg" alt="Free Pixabay Lake Tahoe Picture">
+                	<div class="personalInfo">
+	                	<img class="not_mobile" src="../../../images/dash.png" alt="Custom Dash Word Art">
+	                    <h1>Web Design & Development</h1>
+	                    <img class="not_mobile" src="../../../images/dash.png" alt="Custom Dash Word Art">
+	                    <h2>Christopher David</h2>
+                	</div>
+                </main>
+                <footer>
+                	<div>
+                		<div id="left">
+                			<h3>About</h3>
+                		</div>
+                		<div id="center">
+                			<p>Northern California-born, Christopher David is well acquainted with beautiful sceneries. Lake Tahoe is a specific treasure trove of design inspiration. Christopher is a senior at BYU-I with a few years of web design and development field experience. Graduating in April will open new doors and adventures as he steps into the career field bringing with him a seasoned portfolio of skills and talents. </p>
+                		</div>
+                		<div id="right">
+                			<a href="/images/Resume1-19-18.pdf" download><img src="../../../images/print.png" alt="Custom Print Resume Icon"></a>
+                			<a href="/images/Resume1-19-18.pdf" download><p>Print Resume</p></a>
+                		</div>
+                	</div>
+                </footer>
+            </div><!--end of content container tag-->
+        </div>
+    </body>
+</html>
