ddb4b9e2c214dd3a5f84c4f293ad8d2b9f4afedc
Christopher David <dav08049@byui.edu>
2018-01-26 11:26:29 -0700

    added php shopping cart pages

--- web/assignments/phpShoppingCart/cart.php
+++ web/assignments/phpShoppingCart/cart.php
@@ -108,9 +108,9 @@ $cost = array("1.25", "5.75", "2.50", "1.99");
                             <tr>
                                 <td colspan="7">Total : <?php echo($total); ?></td>
                             </tr>
-                            <a href="https://vast-savannah-73411.herokuapp.com/assignments/phpShoppingCart/checkout.php" class="checkout"><div>Checkout</div></a>
                         </table>
                     <?php }?>
+                    <a href="https://vast-savannah-73411.herokuapp.com/assignments/phpShoppingCart/checkout.php" class="checkout"><div>Checkout</div></a>
                     </div>
                 </main>
                 <footer class="shopping-cart">
