fb909e32d6a77eeff6ffee3bdf7a03ab99c07f99
Christopher David <dav08049@byui.edu>
2018-02-10 00:06:08 -0700

    adding personal project pages

--- web/assignments/project/library/connections.php
+++ web/assignments/project/library/connections.php
@@ -5,20 +5,32 @@
  */
 
 function projectConnect(){
-    $host = "ec2-54-227-244-122.compute-1.amazonaws.com";
-    $dbname = "d9ijibe1kbt68t";
-    $username = "vuaxwnyfjjaypc";
-    $password = "96503a1bb6e5a2db2058ed7c6f9fbe4e43b157f1637905b89756e6acbfa13d9c";
-    $port = "5433";
-
-    $dsn = "pgsql:host=$host;dbname=$dbname;username=$username;port=$port;password=$password";
+    $con = "d9ijibe1kbt68t host=ec2-54-227-244-122.compute-1.amazonaws.com port=5432 user=vuaxwnyfjjaypc password=96503a1bb6e5a2db2058ed7c6f9fbe4e43b157f1637905b89756e6acbfa13d9c sslmode=require";
 
 
-if($db){
-  echo "Connected <br />".$dbname;
-}else {
-  echo "Not connected";
-}
+    if (!$con) 
+    {
+     echo "Database connection failed.";
+    }
+    else 
+    {
+     echo "Database connection success.";
+    }
+
+//     $host = "ec2-54-227-244-122.compute-1.amazonaws.com";
+//     $dbname = "d9ijibe1kbt68t";
+//     $username = "vuaxwnyfjjaypc";
+//     $password = "96503a1bb6e5a2db2058ed7c6f9fbe4e43b157f1637905b89756e6acbfa13d9c";
+//     $port = "5433";
+
+//     $dsn = "pgsql:host=$host;dbname=$dbname;username=$username;port=$port;password=$password";
+
+
+// if($db){
+//   echo "Connected <br />".$dbname;
+// }else {
+//   echo "Not connected";
+// }
     // $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
     // try {
