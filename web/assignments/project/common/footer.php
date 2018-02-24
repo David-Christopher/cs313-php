be63c832efcae0f89f648f73c354dd7f3f572517
Christopher David <dav08049@byui.edu>
2018-02-23 18:46:24 -0700

    updated for password

--- web/assignments/project/common/footer.php
+++ web/assignments/project/common/footer.php
@@ -1,2 +1,21 @@
-        <p>&copy; Plumbing Co. All Rights Reserved</p>
-        <p>This is a school project for CS 313</p>
+        <p onmouseover="mouseOver(this)" onmouseout="off1(this)" onmousedown="mousedown(this)" onmouseup="mouseup(this)">&copy; Plumbing Co. All Rights Reserved</p>
+        <p onmouseover="mouseOver(this)" onmouseout="off1(this)" onmousedown="mousedown(this)" onmouseup="mouseup(this)">This is a school project for CS 313</p>
+        
+        <script type="text/javascript">
+        function mouseOver(obj) {
+		    obj.style.backgroundColor = '#b30000';
+		    obj.style.fontSize = '26px';
+		}
+
+		function off1(obj) {
+		   	obj.style.fontSize = '14px'; 
+		}	
+
+		function mousedown(obj) {
+	    	obj.style.backgroundColor = 'yellow';
+		}
+		
+		function mouseup(obj) {
+		    obj.style.backgroundColor = 'none';
+		}	
+        </script>
